ea999d8f1455e2e83573924b9722ee24ebfd7cee
soufiene <soufiene.katet@outlook.com>
2020-02-09 12:07:30 +0000

    add project

---
+++ GulpJsHintJsSc/Scripts/Js/site.js
@@ -0,0 +1,17 @@
+﻿function validateForm() {
+
+    const awesome = () => console.log('start  validateForm ');
+
+    var x = document.forms.myForm.fname.value;
+    if (x === '') {
+        alert('First Name must be filled out');
+        return false;
+    }
+
+    var y = document.forms.myForm.lname.value;
+    if (y === '') {
+        alert('Last Name must be filled out');
+        return false;
+    }
+}
+
---
+++ GulpJsHintJsSc/gulpfile.js
@@ -0,0 +1,28 @@
+﻿var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var eslint = require('gulp-eslint');
+
+gulp.task('jscs', function () {
+    return gulp.src(['**/*.js', '!node_modules/**'])
+        .pipe(jscs())
+        .pipe(jscs.reporter());
+});
+
+gulp.task('lint', function () {
+    return gulp.src(['**/*.js', '!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('jshint', function () {
+    console.log('analyse my source');
+    return gulp.src(['**/*.js', '!node_modules/**'])      
+        .pipe(jshint())
+        //.pipe(eslint())
+        .pipe(jshint.reporter('jshint-stylish', { verbose: true }));
+});
+
+
+
